5d3c9a1f0509434dee2b02df924389a9d2e7070e
olavhl <ohalar@gmail.com>
2021-05-19 15:41:26 +0200

    Fixing merge conflict

--- smidig-prosjekt-11/src/views/AdminPage.vue
+++ smidig-prosjekt-11/src/views/AdminPage.vue
@@ -6,7 +6,7 @@
 
 <script>
 // @ is an alias to /src
-import BaseSite from '@/components/UI/BaseSite.vue';
+import BaseSite from '@/components/Nav/BaseSite.vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 
