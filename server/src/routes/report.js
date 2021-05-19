413fb3727f089c4b0345fa341781698143accdd8
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-05-19 15:13:00 +0200

    added formating to the inputs

--- server/src/routes/report.js
+++ server/src/routes/report.js
@@ -1,38 +1,65 @@
 const express = require("express");
 const monk = require("monk");
+const Joi = require("joi");
+const { x } = require("joi");
+const { compareSync } = require("bcrypt");
 
 const router = express.Router();
 const db = monk(process.env.MONGO_URI);
+db.then(() => {
+  console.log("connection success");
+}).catch((e) => {
+  console.error("Error !", e);
+});
 
 const report = db.get("report");
-const users = db.get("users");
 
+const schema = Joi.array().items(
+  Joi.object({
+    serialNumber: Joi.string().alphanum().required(),
+    partName: Joi.string().alphanum().min(1).required(),
+    createdAt: Joi.date().default(Date.now),
+    location: Joi.string().required(),
+    parts: Joi.array().items({
+      partNumber: Joi.string().alphanum().min(1).required(),
+      partName: Joi.string().alphanum().min(1).required(),
+    }),
+  })
+);
+router.post("/report", async (req, res) => {
+  if (!req.user) return res.status(401).send();
 
-router.post("/report",async (req, res) => {
-    console.log(JSON.stringify(req.user))
-
-//    if (!req.user) return res.status(401).send();
-//TODO: fix sewssion login to validate that that the is logged in before sending report
-    //body example
-    /*{
-
-        "username" :"terst",
-        "serialNumbers": "fddsfs",
-        "modalTextBody": "sdfsf",
-        "inputSerial" : "fsdfs",
-        "parts":{
-        "partNumber":"fdsd",
-            "partName": "fcsd",
-            "imgName": "sadf"
-    }*/
-
-    const reportData = req.body;
+  //body example
+  /*
+[
+	{
+		"serialNumber": "12345",
+		"partName": "SunBell",
+    "location": "Oslo,Norway",
+		"parts": [
+			{
+				"partNumber": "1",
+				"partName": "PCB"
+			}
+		]
+	}
+]
+  */
+  try {
+    value = await schema.validateAsync(req.body);
+    const data = value.map((x) => {
+      return { ...x, username: req.user.username };
+    });
+    console.log(JSON.stringify(data));
     try {
-        await report.insert(reportData);
-        res.sendStatus(200);
-    }catch (e){
-        res.status(500).send(e);
+      await report.insert(data);
+      res.sendStatus(200);
+    } catch (e) {
+      res.status(500).send(e);
     }
-})
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
 
 module.exports = router;
