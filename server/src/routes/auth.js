f7788d53be78673836688cb321d3d94c212e429c
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-05-19 15:18:25 +0200

    added admin to user

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -17,6 +17,7 @@ const users = db.get("users");
 const schema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{6,30}$")),
+  admin: Joi.boolean().default(false),
 });
 
 //Local login route -- Authenticates with passport and bcrypt for password hashing/unh
