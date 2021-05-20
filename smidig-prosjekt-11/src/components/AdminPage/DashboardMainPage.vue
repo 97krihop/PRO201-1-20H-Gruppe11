e47ed491686548f2432166628c119d684f86c288
glorytiger <chrnor3@gmail.com>
2021-05-20 15:24:50 +0200

    Added map component

--- smidig-prosjekt-11/src/components/AdminPage/DashboardMainPage.vue
+++ smidig-prosjekt-11/src/components/AdminPage/DashboardMainPage.vue
@@ -10,20 +10,46 @@
     data-to-display="1.000.000"
   />
   <top-metric :name-of-data="mostRepairedPartToday" data-to-display="PCB" />
+  
+  <l-map style="height: 350px" :zoom="zoom" :center="center">
+    <l-geo-json :geojson="geojson" :options-style="styleFunction"></l-geo-json>
+    <l-marker :icon="defaultIcon" :lat-lng="markerLatLng">
+      <l-popup>I'm a refugee camp!</l-popup>
+    </l-marker>
+  </l-map>
 </template>
 
 <script>
 import TopMetric from "@/components/AdminPage/TopMetrics";
+import customGeojson from "./data/custom.geo.json";
+import "leaflet/dist/leaflet.css";
+import { LMap, LGeoJson } from "@vue-leaflet/vue-leaflet";
 
 export default {
   name: "DashboardPage",
   setup() {},
   components: {
-    TopMetric
+    TopMetric,
+    LMap,
+    LGeoJson
   },
   methods: {},
   data() {
-    return {};
+    return {
+      geojson: customGeojson,
+      geojsonOptions: {
+        // Options that don't rely on Leaflet methods.
+      }
+    };
+  },
+  async beforeMount() {
+    // HERE is where to load Leaflet components!
+    const { circleMarker } = await import("leaflet/dist/leaflet-src.esm");
+
+    // And now the Leaflet circleMarker function can be used by the options:
+    this.geojsonOptions.pointToLayer = (feature, latLng) =>
+      circleMarker(latLng, { radius: 8 });
+    this.mapIsReady = true;
   }
 };
 </script>
