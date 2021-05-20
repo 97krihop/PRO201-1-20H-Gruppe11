efa893847a414d3c94478f3e086ddd7ceab3488a
Andreas Jevnaker <andreasjevn@gmail.com>
2021-05-20 11:15:03 +0200

    Just missing a way to conditionally render component besides navigation panel

---
+++ smidig-prosjekt-11/src/components/AdminPage/DashboardPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div><h3>Camp Data Page</h3></div></template
+>
+
+<script>
+export default {
+    name: 'DashboardPage',
+    setup() {},
+    components: {},
+    methods: {},
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped></style>
--- smidig-prosjekt-11/src/views/AdminPage.vue
+++ smidig-prosjekt-11/src/views/AdminPage.vue
@@ -1,14 +1,19 @@
 <template>
     <div v-if="isAdmin === true">
         <base-site>
-            <side-bar-menu />
-        </base-site>
+            <side-bar-menu v-on:childToParent="recievedClickInChildSideMenu" />
+            <template>
+                <camp-data-page></camp-data-page>
+            </template>
+            ></base-site
+        >
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import BaseSite from '@/components/Nav/BaseSite.vue';
+import CampDataPage from '@/components/AdminPage/CampDataPage';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 import SideBarMenu from '@/components/AdminPage/SideBarMenu';
@@ -25,6 +30,7 @@ export default {
     },
     components: {
         SideBarMenu,
+        CampDataPage,
         BaseSite
     },
     methods: {
@@ -35,10 +41,14 @@ export default {
             } else {
                 return true;
             }
+        },
+        recievedClickInChildSideMenu(event) {
+            this.selectedSection = event;
         }
     },
     data() {
         return {
+            selectedSection: '',
             isAdmin: this.retrieveIsAdmin()
         };
     }
