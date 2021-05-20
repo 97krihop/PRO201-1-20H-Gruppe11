5a9bdb6193eaeeadf96d29f1ee1434edec3fc6d7
olavhl <ohalar@gmail.com>
2021-05-20 15:56:07 +0200

    Redoing hover&active on homeselect

--- smidig-prosjekt-11/src/components/Nav/HomeSelect.vue
+++ smidig-prosjekt-11/src/components/Nav/HomeSelect.vue
@@ -72,7 +72,7 @@ export default {
   box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.02),
     0 6.7px 5.3px rgba(0, 0, 0, 0.028), 0 12.5px 10px rgba(0, 0, 0, 0.035),
     0 22.3px 17.9px rgba(0, 0, 0, 0.042), 0 41.8px 33.4px rgba(0, 0, 0, 0.05),
-    0 100px 80px rgba(0, 0, 0, 0.07), -2px -3px #899599;
+    0 100px 80px rgba(0, 0, 0, 0.07), -2px -3px #585858;
 }
 
 .button {
@@ -87,17 +87,19 @@ export default {
   height: 6vh;
   margin: 5% 10% auto 5%;
   outline: none;
-  box-shadow: -1px -2px #899599;
+  transition: 150ms;
 
   &:hover {
-    background-color: #c0f6ae;
+    background-color: #959595;
+    color: #ececec;
+    transform: scale(1.01);
   }
 
   &:active {
-    transform: scale(0.98);
+    transform: scale(1);
     transition-duration: 10ms;
-    box-shadow: inset 2px 2px 2px #899599;
-    background-color: #d6ffcb;
+    box-shadow: inset 2px 2px 1px #4d5050;
+    background-color: #6d6d6d;
   }
 
   h4 {
