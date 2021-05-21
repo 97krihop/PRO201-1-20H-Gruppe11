947f467d7b08716c78d364954d0d0623528232fa
olavhl <ohalar@gmail.com>
2021-05-21 14:25:31 +0200

    Starting on styling create user in admin

--- smidig-prosjekt-11/src/components/AdminPage/UserAdministrationPage.vue
+++ smidig-prosjekt-11/src/components/AdminPage/UserAdministrationPage.vue
@@ -3,44 +3,46 @@
     <form @submit="onSubmit">
       <div class="wrapper">
         <div class="input">
-          <span>Username : </span>
+          <label>Username: </label>
           <input
             v-model="username"
             type="text"
-            placeholder="username"
+            placeholder="Enter Username"
             required
           />
           <br />
           <span>{{ usernameError }}</span>
         </div>
         <div class="input">
-          <span>Password : </span>
+          <label>Password: </label>
           <input
             v-model="password"
             type="password"
-            placeholder="password"
+            placeholder="Enter Password"
             required
           />
           <br />
           <span>{{ passwordError }}</span>
         </div>
         <div class="input">
-          <span>confirm password : </span>
+          <label>Confirm Password: </label>
           <input
             v-model="confirmPassword"
             type="password"
-            placeholder="password"
+            placeholder="Enter Password"
             required
           />
           <br />
           <span>{{ CPError }}</span>
         </div>
-        <div class="input">
-          <span>admin : </span>
+        <div class="admin-check">
+          <label>Admin: </label>
           <input v-model="admin" type="checkbox" />
         </div>
         <div>
-          <button type="submit" :disabled="isSubmitting">Submit</button>
+          <button id="submit-btn" type="submit" :disabled="isSubmitting">
+            Submit
+          </button>
         </div>
       </div>
     </form>
@@ -115,17 +117,71 @@ export default {
   flex-wrap: wrap;
   width: 100%;
   height: 100%;
-}
 
-.wrapper {
-  background: #7eb46b;
-  padding: 2rem;
-  display: flex;
-  align-items: center;
-  justify-items: center;
-  flex-direction: column;
-  flex-wrap: wrap;
-  width: 100%;
+  .wrapper {
+    background: #7eb46b;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    width: 100%;
+    border-radius: 5px;
+    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.02),
+      0 6.7px 5.3px rgba(0, 0, 0, 0.028), 0 12.5px 10px rgba(0, 0, 0, 0.035),
+      0 22.3px 17.9px rgba(0, 0, 0, 0.042), 0 41.8px 33.4px rgba(0, 0, 0, 0.05),
+      0 100px 80px rgba(0, 0, 0, 0.07), -2px -3px #75a167;
+  }
+
+  .input {
+    margin-bottom: 1vh;
+    width: 400px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-items: center;
+
+    label {
+      color: #2c2a29;
+      font-weight: bold;
+      width: 200px;
+    }
+
+    input {
+      width: 200px;
+      padding: 0.4em;
+      box-shadow: inset -2px -2px #899599;
+      outline: none;
+    }
+
+    span {
+      width: 100%;
+      font-size: 14px;
+    }
+  }
+
+  #submit-btn {
+    margin-top: 50px;
+    background-color: #ececec;
+    color: #2c2a29;
+    width: 100%;
+    padding: 0.5rem;
+    border-radius: 5px;
+    box-shadow: -1px -2px #899599;
+    outline: none;
+    transition: 150ms;
+
+    &:hover {
+      background-color: #4d5050;
+      color: #ececec;
+    }
+
+    &:active {
+      transform: scale(0.98);
+      transition-duration: 10ms;
+      box-shadow: inset 2px 2px 2px #899599;
+      background-color: #6d6d6d;
+    }
+  }
 }
 
 button {
