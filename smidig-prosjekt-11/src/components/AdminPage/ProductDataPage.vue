1d1691bb191e9c93f28275f8e2b99103f19dc253
olavhl <ohalar@gmail.com>
2021-05-24 11:38:58 +0200

    Initial commit for work on partsoverview

--- smidig-prosjekt-11/src/components/AdminPage/ProductDataPage.vue
+++ smidig-prosjekt-11/src/components/AdminPage/ProductDataPage.vue
@@ -1,6 +1,17 @@
 <template>
-  <div><h3>Product Data Page</h3></div></template
->
+  <div>
+    <h3>Product Data Page</h3>
+    <ul>
+      <li>Nøkkeldata:</li>
+      <li>Most changed parts</li>
+      <li>Camp with the most changed parts</li>
+      <li>Most repaired parts today</li>
+      <li>Most repaired part monthly</li>
+    </ul>
+    <br />
+    <h3>History</h3>
+  </div>
+</template>
 
 <script>
 export default {
