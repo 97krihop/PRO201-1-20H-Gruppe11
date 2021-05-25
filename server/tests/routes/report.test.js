d7164a77e5f7f919c36b733ffd46278566a09eb0
WhoCanThisBe <Wen.hhp@gmail.com>
2021-05-25 11:33:29 +0200

    add mockup function to report

--- server/tests/routes/report.test.js
+++ server/tests/routes/report.test.js
@@ -1,22 +1,10 @@
 const body =  [
-	{
-		"serialNumber": "12345",
-		"partName": "SunBell",
-		"location": {
-			"name":"oslo, norway",
-			"type": "Point",
-		},
-		"parts": [
-			{
-				"partNumber": "1",
-				"partName": "PCB"
-			}
-		]
-	}
-];
+]
+
+const productName = `"productName": "SunBell"`;
 
 
-
+//SerialNumber
 
 function randomize(){
     Math.floor(Math.random() * 10)
@@ -27,24 +15,92 @@ const serialInput = () =>{
     return `"serialNumber":  "${randomize}${randomize}${randomize}${randomize}"`
 }
 
+
+
+//Camp name
+
+const campName = [
+	"Yida Refugee Camp",
+	"Pugnido Refugee Camp",
+	"Katumba Refugee Camp",
+	"Kakuma Refugee Camp",
+	"Hagadera Refugee Camp"
+]
+
+
+const randCampInput = () =>{
+	const index = Math.floor(Math.random() * campName.length)
+	return `
+		"campName": "${campName[index]}",
+	`
+}
+
 //Partname
 
 
-const parts =  [
-	"Lamp",
-	"12V charger",
-	"Battery",
-	"Power button",
-	"Light bulb",
-	"Screen",
-	"Socket charger",
-	"Solar panel"
+const parts = [
+	{
+		productName: "SunBell",
+		partNumber: "1",
+		partName: "Lamp",
+	},
+	{
+		productName: "SunBell",
+		partNumber: "2",
+		partName: "12V charger",
+	},
+	{
+		productName: "SunBell",
+		partNumber: "3",
+		partName: "Battery",
+	},
+	{
+		productName: "SunBell",
+		partNumber: "4",
+		partName: "Power button",
+	},
+	{
+		productName: "SunBell",
+		partNumber: "5",
+		partName: "Light bulb",
+	},
+	{
+		productName: "SunBell",
+		partNumber: "6",
+		partName: "Screen",
+	},
+	{
+		productName: "SunBell",
+		partNumber: "7",
+		partName: "Socket charger",
+
+	},
+	{
+		productName: "SunBell",
+		partNumber: "8",
+		partName: "Solar panel",
+	}
 ]
 
-const randPart = () => {
+const randPartInput = () => {
 	const index = Math.floor(Math.random() * parts.length);
 
-	return `"partName" : "${parts[index]}"`
+	return `"parts" : [
+	"partNumber": "${parts[index].partNumber}"
+	"partName" : "${parts[index].partName}"
+	]`
+}
+
+
+for(let i = 0; i<100 ; i++){
+	let report = {
+		serialInput,
+		productName,
+		randCampInput,
+		randPartInput
+	}
+
+	body.push(report)
 }
 
 
