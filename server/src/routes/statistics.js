f93d5d6c226d40196085235659a6a9ebecde0d93
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-05-25 13:28:15 +0200

    added totalRepairs

--- server/src/routes/statistics.js
+++ server/src/routes/statistics.js
@@ -5,4 +5,11 @@ const db = require("../db/mongo");
 
 const report = db.get("report");
 
+router.get("/totalRepairs", async (req, res) => {
+  if (!req.user || !req.user.admin) return res.status(401).send();
+
+  const count = await report.count({});
+  res.json({ totalRepairs: count });
+});
+
 module.exports = router;
