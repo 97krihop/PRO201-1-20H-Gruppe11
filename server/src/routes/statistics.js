feffc29c72b81d0280f38506e2ba9903a711d976
97krihop <24739853+97krihop@users.noreply.github.com>
2021-05-25 14:30:20 +0200

    folder structure (#77)
    Co-authored-by: Andreas Jevnaker <andreasjevn@gmail.com>

--- server/src/routes/statistics.js
+++ server/src/routes/statistics.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 const db = require("../db/mongo");
 
+const { getPartCount } = require("../controllers/PartStatisticsController")
+
+
 const report = db.get("report");
+
+router.get("/partCount", async (req, res) => {
+    //if (!req.user || !req.user.admin) return res.status(401).send();
+
+    const count = await getPartCount("Socket charger")
+    console.log(count)
+    res.json({ totalRepairs: count });
+});
 
 router.get("/totalRepairs", async (req, res) => {
   if (!req.user || !req.user.admin) return res.status(401).send();
