e1218d82b1db5006fefb9cd62816e8f4d026cfa4
97krihop <24739853+97krihop@users.noreply.github.com>
2021-05-25 13:06:23 +0200

    edit name om squima (#71)
    Co-authored-by: WhoCanThisBe <Wen.hhp@gmail.com>
    Co-authored-by: Andreas Jevnaker <andreasjevn@gmail.com>

--- server/src/app.js
+++ server/src/app.js
@@ -45,8 +45,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //routes
-app.use("/api", require("./routes/report"));
 app.use("/api", require("./routes/auth"));
+app.use("/api/report", require("./routes/report"));
+app.use("/api/camp", require("./routes/camp"));
 app.use("/api", require("./routes/test"));
 
 app.get("/", (req, res) => {
