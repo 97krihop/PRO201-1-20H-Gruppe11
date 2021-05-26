983c31ff7d070433cb1964074f184913e42061af
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-05-26 14:35:39 +0200

    added default admin user

--- server/src/db/mongo.js
+++ server/src/db/mongo.js
@@ -1,4 +1,5 @@
 const monk = require("monk");
+const bcrypt = require("bcrypt");
 
 const db = monk(process.env.MONGO_URI);
 db.then(() => {
@@ -6,5 +7,19 @@ db.then(() => {
 }).catch((e) => {
   console.error("Error !", e);
 });
+
+const addAdmin = async () => {
+  const user = db.get("users");
+  if (await user.find({})) {
+    const password = await bcrypt.hash("admin", 10);
+    user.insert({
+      username: "bright",
+      password: password,
+      admin: true,
+      campName: "Bright",
+    });
+  }
+};
+addAdmin();
 
 module.exports = db;
