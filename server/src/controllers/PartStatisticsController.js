e24abb162992189c050c4302fc57210eeabe5f4c
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-05-26 13:46:08 +0200

    better mode to wright querys

--- server/src/controllers/PartStatisticsController.js
+++ server/src/controllers/PartStatisticsController.js
@@ -2,10 +2,12 @@ const db = require("../db/mongo");
 const reports = db.get("report");
 
 const getPartCountByPartName = async (param) => {
-  return reports.count({ parts: { $elemMatch: { partName: param } } });
+  //reports.count({ parts: { $elemMatch: { partName: param } } });
+  return reports.count({ "parts.partName": param });
 };
 const getPartCountByPartNumber = async (param) => {
-  return reports.count({ parts: { $elemMatch: { partNumber: param } } });
+  //reports.count({ parts: { $elemMatch: { partNumber: param } } });
+  return reports.count({ "parts.partNumber": param });
 };
 
 module.exports = { getPartCountByPartName, getPartCountByPartNumber };
