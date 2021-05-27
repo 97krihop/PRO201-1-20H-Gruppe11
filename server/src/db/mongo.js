ac05b6ed00a2800fed657ed58cc51af2dc1712b4
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-05-27 15:22:31 +0200

    added tests for login register

--- server/src/db/mongo.js
+++ server/src/db/mongo.js
@@ -6,11 +6,12 @@ db.then(async () => {
   console.log("connection success");
   await addAdmin();
 }).catch((e) => {
-  console.error("Error !", e);
+  console.error("Error with monk");
 });
 
 const addAdmin = async () => {
   const user = db.get("users");
+  console.log("test");
   if (await user.find({})) {
     const password = await bcrypt.hash("admin", 10);
     user.insert({
