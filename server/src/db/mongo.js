90c25ccd56d017a84d20aa2c7ab694c3d6746ea0
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-06-02 09:18:20 +0200

    made addAdmin actuly work

--- server/src/db/mongo.js
+++ server/src/db/mongo.js
@@ -11,13 +11,15 @@ db.then(async () => {
 
 const addAdmin = async () => {
   const user = db.get("users");
-  if (await user.find({})) {
+  const data = await user.find({});
+  console.log(data.length)
+  if (data.length === 0) {
     const password = await bcrypt.hash("admin", 10);
     user.insert({
       username: "bright",
       password: password,
       admin: true,
-      campName: "Bright",
+      campName: "Bright"
     });
   }
 };
